c73b2b716aafe65dd0e4b1963d2a1fceeeed7e88
Łukasz Piątkowski <piontec@gmail.com>
2022-10-06 13:29:23 +0200

    fix the smoke tests

--- tests/ats/test_smoke.py
+++ tests/ats/test_smoke.py
@@ -5,6 +5,7 @@ import pykube
 import pytest
 
 from pytest_helm_charts.clusters import Cluster
+from pytest_helm_charts.k8s.deployment import wait_for_deployments_to_run
 
 logger = logging.getLogger(__name__)
 
@@ -26,8 +27,17 @@ def test_api_working(kube_cluster: Cluster) -> None:
 
 @pytest.mark.smoke
 def test_pods_available(
-    kube_cluster: Cluster, deployments: List[pykube.Deployment]
+    kube_cluster: Cluster
 ) -> None:
+    deployments = wait_for_deployments_to_run(
+        kube_cluster.kube_client,
+        [
+            "crossplane",
+            "crossplane-rbac-manager",
+        ],
+        "default",
+        90,
+    )
     for d in deployments:
         assert int(d.obj["status"]["readyReplicas"]) > 0
         logger.info(f"Deployment '{d.name}' is ready")
